11b4c3f1057238f393faaf7307ada592b0d7fb6c
YusupovArtur <jusupov.artur24@gmail.com>
2025-08-04 21:44:30 +0500

    Sorting context without undefined props

--- src/widgets/TransactionsSortingFilteringMenu/hooks/useTransactionsSortingContext.ts
+++ src/widgets/TransactionsSortingFilteringMenu/hooks/useTransactionsSortingContext.ts
@@ -2,15 +2,10 @@ import { Dispatch, SetStateAction } from 'react';
 import { TransactionsSortingOrderType } from 'widgets/TransactionsSortingFilteringMenu/types/TransactionsSortingOrderType.ts';
 import { useContextFactory } from 'shared/hooks/useContextFactory.tsx';
 
-type TransactionsSortingContextType =
-  | {
-      sortingOrder: TransactionsSortingOrderType;
-      setSortingOrder: Dispatch<SetStateAction<TransactionsSortingOrderType>>;
-    }
-  | {
-      sortingOrder: undefined;
-      setSortingOrder: undefined;
-    };
+type TransactionsSortingContextType = {
+  sortingOrder: TransactionsSortingOrderType;
+  setSortingOrder: Dispatch<SetStateAction<TransactionsSortingOrderType>>;
+};
 
 const { Context: TransactionsSortingContext, useMyContextWithoutFallback: useTransactionsSortingContext } =
   useContextFactory<TransactionsSortingContextType>('useTransactionsSortingContext');
