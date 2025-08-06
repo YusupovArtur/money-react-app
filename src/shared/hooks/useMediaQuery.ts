ab4387050e9fbd7f372fd357523e51135cd42f5c
YusupovArtur <jusupov.artur24@gmail.com>
2025-08-06 20:09:27 +0500

    useMediaQuery.ts refactored to using useLayoutEffect

--- src/shared/hooks/useMediaQuery.ts
+++ src/shared/hooks/useMediaQuery.ts
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 
 export const useMediaQuery = (query: string): boolean => {
   const [matches, setMatches] = useState(false);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const mediaQuery = window.matchMedia(query);
     setMatches(mediaQuery.matches);
 
